8ef5087a0906159272e7dfb05b4f630bb2499a15
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-05-31 11:25:56 +0530

    Update Connection.js

--- Config/Connection.js
+++ Config/Connection.js
@@ -1,59 +1,59 @@
 //local db
-import mysql from "mysql2";
+//import mysql from "mysql2";
 
-const dbConfig = {
-  host: '127.0.0.1',
-  port: 3306,
-  port: process.env.DBPORT,
-  user: "root",
-  password:'',
-  database: "google-review",
-  charset: "utf8mb4",
-  multipleStatements: false,
-  timezone: "Z",
+//const dbConfig = {
+//  host: '127.0.0.1',
+//  port: 3306,
+//  port: process.env.DBPORT,
+//  user: "root",
+//  password:'',
+//  database: "google-review",
+//  charset: "utf8mb4",
+//  multipleStatements: false,
+//  timezone: "Z",
 
-};
+//};
 
-const pool = mysql.createPool(dbConfig);
+//const pool = mysql.createPool(dbConfig);
 
-pool.promise()
-  .getConnection()
-  .then((connection) => {
-    console.log("Database connected successfully");
-    connection.release(); 
-  })
-  .catch((err) => {
-    console.error("Error connecting to the database:", err.message);
-  });
+//pool.promise()
+ // .getConnection()
+ // .then((connection) => {
+ //   console.log("Database connected successfully");
+ //   connection.release(); 
+ // })
+ // .catch((err) => {
+ //   console.error("Error connecting to the database:", err.message);
+ // });
 
-export default pool.promise();
+//export default pool.promise();
 
 
 // live db
 
-// import mysql from "mysql2";
+ import mysql from "mysql2";
 
-// const dbConfig = {
-//   host: "tramway.proxy.rlwy.net",
-//   port: 43491,
-//   user: "root",
-//   password: "EBhJaMfFUSXyyMuXGUrXJEikHRWXNuwM",
-//   database: "railway",
-//   charset: "utf8mb4",
-//   multipleStatements: false,
-//   timezone: "Z",
-// };
+ const dbConfig = {
+   host: "hopper.proxy.rlwy.net",
+   port: 38129,
+   user: "root",
+   password: "QADhthIBrZIhcOIIzVFoFeqpmzBcwCni",
+   database: "railway",
+   charset: "utf8mb4",
+   multipleStatements: false,
+   timezone: "Z",
+ };
 
-// const pool = mysql.createPool(dbConfig);
+ const pool = mysql.createPool(dbConfig);
 
-// pool.promise()
-//   .getConnection()
-//   .then((connection) => {
-//     console.log("Database connected successfully");
-//     connection.release();
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to the database:", err.message);
-//   });
+ pool.promise()
+   .getConnection()
+   .then((connection) => {
+     console.log("Database connected successfully");
+     connection.release();
+   })
+   .catch((err) => {
+     console.error("Error connecting to the database:", err.message);
+   });
 
-// export default pool.promise();
+ export default pool.promise();
