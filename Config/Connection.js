80c339c9a51c949756c522d8a9dd78d25940682e
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-07-24 12:56:56 +0530

    d

--- Config/Connection.js
+++ Config/Connection.js
@@ -1,44 +1,13 @@
-//local db
-//import mysql from "mysql2";
 
-//const dbConfig = {
-//  host: '127.0.0.1',
-//  port: 3306,
-//  port: process.env.DBPORT,
-//  user: "root",
-//  password:'',
-//  database: "google-review",
-//  charset: "utf8mb4",
-//  multipleStatements: false,
-//  timezone: "Z",
-
-//};
-
-//const pool = mysql.createPool(dbConfig);
-
-//pool.promise()
- // .getConnection()
- // .then((connection) => {
- //   console.log("Database connected successfully");
- //   connection.release(); 
- // })
- // .catch((err) => {
- //   console.error("Error connecting to the database:", err.message);
- // });
-
-//export default pool.promise();
-
-
-// live db
 
  import mysql from "mysql2";
 
  const dbConfig = {
-   host: "hopper.proxy.rlwy.net",
-   port: 38129,
-   user: "root",
-   password: "QADhthIBrZIhcOIIzVFoFeqpmzBcwCni",
-   database: "railway",
+   host: "147.93.30.189",
+   port: 3306,
+   user: "alex",
+   password: "Root@123google",
+   database: "rateo",
    charset: "utf8mb4",
    multipleStatements: false,
    timezone: "Z",
@@ -53,7 +22,7 @@
      connection.release();
    })
    .catch((err) => {
-     console.error("Error connecting to the database:", err.message);
+     console.error("Error connecting to the database:", err);
    });
 
  export default pool.promise();
